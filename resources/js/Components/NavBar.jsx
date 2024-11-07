0d964520c87cb569db364427a1ad48293d0bcb60
abyankamal <abyankamal8@gmail.com>
2024-11-07 17:15:04 +0700

    fix navbar

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -55,10 +55,10 @@ const Navbar = ({ auth }) => {
 
                 {/* Buttons */}
                 <div className="ml-4 space-x-4">
-                    <button className="bg-white hover:bg-lime-400 ring-green-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded">
+                    <button className="bg-white hover:bg-lime-400 ring-lime-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded">
                         Sign Up
                     </button>
-                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded">
                         Sign In
                     </button>
                 </div>
