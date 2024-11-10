53f2a688d0a364781e4582f74b6d36d06049698e
abyankamal <abyankamal8@gmail.com>
2024-11-10 21:26:13 +0700

    change testimonial card

--- resources/js/Components/Testimonials.jsx
+++ resources/js/Components/Testimonials.jsx
@@ -4,7 +4,7 @@ export default function Testimonials() {
     return (
         <div className="bg-lime-500 my-16 py-10">
             <h2 className="text-center text-white text-5xl lg:mb-16 font-semibold mb-6">
-                Trusted By 1000++ Students
+                Dipercaya lebih dari 900k pelajar
             </h2>
             <div className="flex justify-center space-x-4">
                 <TestimonialCard
@@ -15,6 +15,12 @@ export default function Testimonials() {
                     userRole="UI/UX Designer"
                 />
             </div>
+            <button
+                type="button"
+                className="rounded-full bg-lime-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
+            >
+                Button text
+            </button>
         </div>
     );
 }
