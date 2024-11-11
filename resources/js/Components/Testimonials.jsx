e59d8fccff42da7d104d52dea3a89fdc6f6a8e6f
abyankamal <abyankamal8@gmail.com>
2024-11-11 12:43:39 +0700

    change testimonial card

--- resources/js/Components/Testimonials.jsx
+++ resources/js/Components/Testimonials.jsx
@@ -14,13 +14,28 @@ export default function Testimonials() {
                     userName="Evita"
                     userRole="UI/UX Designer"
                 />
+                <TestimonialCard
+                    title="Hemat Waktu"
+                    description="Materi design & development mudah dipahami, thanks BuildWithAngga."
+                    userImage="https://via.placeholder.com/72" // Replace with the actual user image URL
+                    userName="Evita"
+                    userRole="UI/UX Designer"
+                />
+                <TestimonialCard
+                    title="Hemat Waktu"
+                    description="Materi design & development mudah dipahami, thanks BuildWithAngga."
+                    userImage="https://via.placeholder.com/72" // Replace with the actual user image URL
+                    userName="Evita"
+                    userRole="UI/UX Designer"
+                />
+                <TestimonialCard
+                    title="Hemat Waktu"
+                    description="Materi design & development mudah dipahami, thanks BuildWithAngga."
+                    userImage="https://via.placeholder.com/72" // Replace with the actual user image URL
+                    userName="Evita"
+                    userRole="UI/UX Designer"
+                />
             </div>
-            <button
-                type="button"
-                className="rounded-full bg-lime-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
-            >
-                Button text
-            </button>
         </div>
     );
 }
