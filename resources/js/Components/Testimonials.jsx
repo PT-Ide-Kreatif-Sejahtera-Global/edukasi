7f05ef20d83301337574dcf0784ff1c1b5993d09
abyankamal <abyankamal8@gmail.com>
2024-11-14 21:51:49 +0700

    update testimonials

--- resources/js/Components/Testimonials.jsx
+++ resources/js/Components/Testimonials.jsx
@@ -2,10 +2,12 @@ import { TestimonialCard } from "./ui/TestimonialCard";
 
 export default function Testimonials() {
     return (
-        <div className="grid grid-cols-2 bg-lime-500 my-16 p-10 gap-6">
-            <div className=" bg-white shadow sm:rounded-lg h-64 m-auto">
+        <div className="grid grid-rows-2 lg:grid-cols-2 bg-lime-500 my-16 p-10 gap-6">
+            <div className=" bg-white shadow sm:rounded-lg h-64 m-auto w-96">
                 <div className="px-4 py-5 sm:p-6 items-center">
-                    <p>Dipercaya oleh lebih dari 900k pelajar</p>
+                    <p className="text-lime-500 text-2xl font-medium">
+                        Dipercaya oleh lebih dari 900k pelajar
+                    </p>
                     <h3 className="text-lg font-medium text-gray-900">
                         Testimonial
                     </h3>
--- resources/js/Components/ui/TestimonialCard.jsx
+++ resources/js/Components/ui/TestimonialCard.jsx
@@ -5,16 +5,16 @@ export const TestimonialCard = ({
     userName,
 }) => {
     return (
-        <div className="bg-white shadow-lg rounded-xl p-6 w-64 h-auto max-w-sm mx-auto text-gray-700">
+        <div className="bg-white shadow-lg rounded-xl p-6 mx-auto text-gray-700 flex flex-col">
             <h3 className="text-xl font-bold mb-2">{title}</h3>
             <p className="text-gray-600 mb-4">{description}</p>
-            <div className="flex items-center">
+            <div className="flex items-center mt-4">
                 <img
                     src={userImage}
                     alt={userName}
                     className="w-12 h-12 rounded-full mr-3"
                 />
-                <div>
+                <div className="flex flex-col">
                     <p className="font-semibold">{userName}</p>
                 </div>
             </div>
