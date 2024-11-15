35cc2eeddcadeedc4b8b8d7e2df537c2d760762e
abyankamal <abyankamal8@gmail.com>
2024-11-15 14:23:53 +0700

    update testimonials

--- resources/js/Components/Testimonials.jsx
+++ resources/js/Components/Testimonials.jsx
@@ -2,19 +2,21 @@ import { TestimonialCard } from "./ui/TestimonialCard";
 
 export default function Testimonials() {
     return (
-        <div className="grid grid-rows-2 lg:grid-cols-2 bg-lime-500 my-16 p-10 gap-6">
-            <div className=" bg-white shadow sm:rounded-lg h-64 m-auto w-96">
+        <div className="grid grid-cols-1 sm:grid-cols-5 bg-lime-500 mt-16 md:mt-48 p-10 gap-6">
+            <div className="row-span-2 sm:col-span-2 bg-white shadow-md rounded-lg h-full m-auto w-full">
                 <div className="px-4 py-5 sm:p-6 items-center">
-                    <p className="text-lime-500 text-2xl font-medium">
+                    <p className="text-lime-500 text-md font-medium">
                         Dipercaya oleh lebih dari 900k pelajar
                     </p>
-                    <h3 className="text-lg font-medium text-gray-900">
-                        Testimonial
+                    <h3 className="text-xl font-bold text-gray-900 my-2">
+                        Mari bergabung dengan kami dan rasakan manfaatnya
                     </h3>
-                    <button>Cek Katalog Kelas</button>
+                    <button className="text-white flex items-center text-sm font-medium py-2 px-4 rounded-lg bg-lime-500 hover:bg-lime-400 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2">
+                        Cek Kelas
+                    </button>
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-4 justify-center">
+            <div className="col-span-1 sm:col-span-3 grid grid-cols-2 gap-4">
                 <TestimonialCard
                     title="Hemat Waktu"
                     description="Materi design & development mudah dipahami, thanks IdeaCourse."
--- resources/js/Components/ui/TestimonialCard.jsx
+++ resources/js/Components/ui/TestimonialCard.jsx
@@ -5,7 +5,7 @@ export const TestimonialCard = ({
     userName,
 }) => {
     return (
-        <div className="bg-white shadow-lg rounded-xl p-6 mx-auto text-gray-700 flex flex-col">
+        <div className="bg-white shadow-lg rounded-xl p-6 mx-auto text-gray-700">
             <h3 className="text-xl font-bold mb-2">{title}</h3>
             <p className="text-gray-600 mb-4">{description}</p>
             <div className="flex items-center mt-4">
