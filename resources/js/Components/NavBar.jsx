b41ec56a168536d27fcd3be9a5b4d7f9583be57a
abyankamal <abyankamal8@gmail.com>
2024-11-19 17:55:00 +0700

    change navbar

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -1,71 +1,126 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
 import { Link } from "@inertiajs/react";
 
+library.add(fas);
+
 export default function Navbar({ auth }) {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen((prev) => !prev);
+    };
+
+    const closeDropdown = () => {
+        setDropdownOpen(false);
+    };
+
     return (
-        <nav className="bg-white shadow-lg">
-            <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-                {/* Logo */}
-                <div className="flex items-center">
-                    <img
-                        src="/admin/images/logos/IdeaThings.png"
-                        alt="Logo"
-                        className="ml-4 lg:ml-12 h-16 w-auto"
-                    />
-                </div>
-
-                <div className="ml-4">
-                    <p className="hidden lg:block">Kategori</p>
-                </div>
-
-                {/* Search Bar */}
-                <div className="relative sm:w-60 md:w-96 lg:w-[500px] hidden sm:block px-3 py-2">
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <FontAwesomeIcon
-                            icon={["fas", "search"]}
-                            className="px-4 text-gray-400"
+        <div>
+            <nav className="bg-white shadow-lg">
+                <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+                    {/* Logo */}
+                    <div className="flex items-center">
+                        <img
+                            src="/admin/images/logos/IdeaThings.png"
+                            alt="Logo"
+                            className="ml-4 lg:ml-12 h-16 w-auto"
                         />
-                    </span>
-                    <input
-                        type="text"
-                        className="block w-full pl-10 rounded-full border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                        placeholder="Cari Kursus Anda Disini"
-                    />
-                </div>
+                    </div>
 
-                {/* Cart Icon */}
-                <div className="ml-4">
-                    <p>Pembelajaran Saya</p>
-                </div>
+                    {/* Kategori with dropdown trigger */}
+                    <div className="relative ml-4">
+                        <p
+                            className="cursor-pointer hidden lg:block"
+                            onClick={toggleDropdown}
+                            onMouseEnter={() => setDropdownOpen(true)}
+                            onMouseLeave={closeDropdown} // Close on mouse leave
+                        >
+                            Kategori
+                        </p>
 
-                {/* Cart Icon */}
-                <div className="ml-4">
-                    <FontAwesomeIcon
-                        className="h-6 w-6 text-lime-500 hidden md:block"
-                        icon={["fas", "shopping-cart"]}
-                    />
-                </div>
+                        {/* Dropdown Menu */}
+                        {isDropdownOpen && ( // Render dropdown if open
+                            <div className="absolute left-1/2 transform -translate-x-1/2 z-40 mt-9 w-48 bg-white border rounded-lg shadow-lg">
+                                <Link
+                                    href="/kategori/kategori1"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-lime-100"
+                                    onClick={closeDropdown} // Close dropdown on link click
+                                >
+                                    Kategori 1
+                                </Link>
+                                <Link
+                                    href="/kategori/kategori2"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-lime-100"
+                                    onClick={closeDropdown}
+                                >
+                                    Kategori 2
+                                </Link>
+                                <Link
+                                    href="/kategori/kategori3"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-lime-100"
+                                    onClick={closeDropdown}
+                                >
+                                    Kategori 3
+                                </Link>
+                                <Link
+                                    href="/kategori/kategori4"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-lime-100"
+                                    onClick={closeDropdown}
+                                >
+                                    Kategori 4
+                                </Link>
+                            </div>
+                        )}
+                    </div>
 
-                {/* Buttons */}
-                <div className="ml-4 space-x-4">
-                    <Link
-                        href="/register"
-                        className="bg-white hover:bg-lime-400 ring-lime-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded"
-                    >
-                        Sign Up
-                    </Link>
-                    <Link
-                        href="/login"
-                        className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded"
-                    >
-                        Sign In
-                    </Link>
+                    {/* Search Bar */}
+                    <div className="relative sm:w-60 md:w-96 lg:w-[500px] hidden sm:block px-3 py-2">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <FontAwesomeIcon
+                                icon={["fas", "search"]}
+                                className="px-4 text-gray-400"
+                            />
+                        </span>
+                        <input
+                            type="text"
+                            className="block w-full pl-10 rounded-full border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                            placeholder="Cari Kursus Anda Disini"
+                        />
+                    </div>
+
+                    {/* Cart Icon */}
+                    <div className="ml-4">
+                        <p>Pembelajaran Saya</p>
+                    </div>
+
+                    {/* Cart Icon */}
+                    <div className="ml-4">
+                        <FontAwesomeIcon
+                            className="h-6 w-6 text-lime-500 hidden md:block"
+                            icon={["fas", "shopping-cart"]}
+                        />
+                    </div>
+
+                    {/* Buttons */}
+                    <div className="ml-4 space-x-4">
+                        <Link
+                            href="/register"
+                            className="bg-white hover:bg-lime-400 ring-lime-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded"
+                        >
+                            Sign Up
+                        </Link>
+                        <Link
+                            href="/login"
+                            className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Sign In
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 }
