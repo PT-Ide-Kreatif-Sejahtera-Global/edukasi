970d08c2054ba2a37b6d6d88b774f6dc05dd5a2f
abyankamal <abyankamal8@gmail.com>
2024-11-19 17:38:33 +0700

    change navbar

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -52,12 +52,18 @@ export default function Navbar({ auth }) {
 
                 {/* Buttons */}
                 <div className="ml-4 space-x-4">
-                    <button className="bg-white hover:bg-lime-400 ring-lime-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded">
+                    <Link
+                        href="/register"
+                        className="bg-white hover:bg-lime-400 ring-lime-300 text-lime-400 hover:text-white ring-1 ring-inset font-bold py-2 px-4 rounded"
+                    >
                         Sign Up
-                    </button>
-                    <button className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded">
+                    </Link>
+                    <Link
+                        href="/login"
+                        className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded"
+                    >
                         Sign In
-                    </button>
+                    </Link>
                 </div>
             </div>
         </nav>
