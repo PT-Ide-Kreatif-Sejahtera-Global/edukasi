76439c1aa1cb3925aa6934c26754ef3e135b23f0
abyankamal <abyankamal8@gmail.com>
2024-11-25 11:37:04 +0700

    add user navbar

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -1,11 +1,11 @@
 import Footer from "@/Components/Footer";
-import Navbar from "@/Components/NavBar";
+import UserNavbar from "@/Components/UserNavBar";
 
 export default function UserLayout({ children, auth, purchasedCourses }) {
     return (
         <div className="flex flex-col min-h-screen">
             {/* Navbar */}
-            <Navbar auth={auth} purchasedCourses={purchasedCourses} />
+            <UserNavbar auth={auth} purchasedCourses={purchasedCourses} />
 
             {/* Main Content */}
             <main className="flex-grow">{children}</main>
