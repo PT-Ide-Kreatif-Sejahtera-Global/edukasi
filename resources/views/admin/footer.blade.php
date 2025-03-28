23b299d51bebecd8f09effdecbaa435893ea345d
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-03-28 22:38:37 +0700

    fix bug login 404

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -2,7 +2,7 @@
     <p class="mb-0 fs-4">
         Copyright &copy; {{ date('Y') }}
         <a href="https://ideathings.id/" target="_blank" class="pe-1 text-primary text-decoration-underline">
-            IdeaThings
+            iDeaThings
         </a>
     </p>
 </div>
